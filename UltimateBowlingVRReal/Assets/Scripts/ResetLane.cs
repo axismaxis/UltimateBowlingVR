1d478a4d17601569b25cc4bbb91023feee5f680e
Timon Borg <timon.borg@gmail.com>
2019-06-25 21:31:38 +0200

    Updated project with demo scene for assesment

---
+++ UltimateBowlingVRReal/Assets/Scripts/ResetLane.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLane : MonoBehaviour
+{
+    private CheckPins PinController;
+
+    private bool timeron = false;
+    public float time;
+    private float resettime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PinController = GameObject.FindGameObjectWithTag("PinController").GetComponent<CheckPins>();
+        resettime = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeron)
+        {
+            time -= Time.deltaTime;
+
+            if (time <= 0.0f)
+            {
+                timerEnded();
+                time = resettime;
+            }
+        }
+    }
+
+    void timerEnded()
+    {
+        timeron = false;
+        PinController.BallEnded();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag.Equals("Ball"))
+        {
+            timeron = true;
+            PinController.DestroyBall();
+        }
+    }
+}
